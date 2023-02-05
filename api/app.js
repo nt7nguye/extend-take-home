ebde85a9375b9622e9c3642cc92277e4a7a1fcff
nt7nguye <nt7nguye@uwaterloo.ca>
2023-02-05 17:28:29 -0500

    linking Express and React

--- api/app.js
+++ api/app.js
@@ -3,12 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var cardsRouter = require('./routes/cards');
 var transactionsRouter = require('./routes/transactions');
 
 var app = express();
+
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
