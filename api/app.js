110fa729d5eaf001e18dbc0f5a80304cf4b58f2d
nt7nguye <nt7nguye@uwaterloo.ca>
2023-02-05 17:13:37 -0500

    adding Express routes

--- api/app.js
+++ api/app.js
@@ -5,7 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var cardsRouter = require('./routes/cards');
+var transactionsRouter = require('./routes/transactions');
 
 var app = express();
 
@@ -19,8 +20,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', indexRouter);
+app.use('/api/cards', cardsRouter);
+app.use('/api/transactions', transactionsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
